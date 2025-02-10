709f8659c170a60d2f3f3feef218c743e98e36fc
Thomas Eckert <thomas.eckert@redpanda.com>
2025-02-10 12:10:13 -0500

    Add mini align and table management

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -674,6 +674,8 @@ require("lazy").setup({
 					return "%2l:%-2v"
 				end
 
+				require("mini.align").setup()
+
 				-- ... and there is more!
 				--  Check out: https://github.com/echasnovski/mini.nvim
 			end,
@@ -710,6 +712,13 @@ require("lazy").setup({
 					folder = "./~Templates",
 				},
 			},
+		},
+		-- Markdown Table Formatting
+		{
+			"Kicamon/markdown-table-mode.nvim",
+			config = function()
+				require("markdown-table-mode").setup()
+			end,
 		},
 	},
 	checker = { enabled = true },
