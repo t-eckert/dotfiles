930586e911a207ec2e80ecf67ca9b46de013ca01
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-08-24 23:00:39 -0400

    Prevent double goto def

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -463,21 +463,15 @@ require("lazy").setup({
 						return vim.fn.executable("make") == 1
 					end,
 				},
-				{ "nvim-telescope/telescope-ui-select.nvim" },
 				{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 			},
 			config = function()
 				require("telescope").setup({
-					extensions = {
-						["ui-select"] = {
-							require("telescope.themes").get_dropdown(),
-						},
-					},
+					extensions = {},
 				})
 
 				-- Enable telescope extensions
 				pcall(require("telescope").load_extension, "fzf")
-				pcall(require("telescope").load_extension, "ui-select")
 
 				-- Keybindings
 				local builtin = require("telescope.builtin")
