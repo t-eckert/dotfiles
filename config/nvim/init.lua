e3d66a0469aa55a30e54113cbaae34ed99554df6
Thomas Eckert <thomas.james.eckert@gmail.com>
2026-01-24 00:38:19 -0500

    Add folke/persistence.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1093,6 +1093,43 @@ require("lazy").setup({
       opts = { signs = false },
     },
 
+    -- Session management
+    {
+      "folke/persistence.nvim",
+      event = "BufReadPre",
+      opts = {},
+      keys = {
+        {
+          "<leader>qs",
+          function()
+            require("persistence").load()
+          end,
+          desc = "Restore session",
+        },
+        {
+          "<leader>qS",
+          function()
+            require("persistence").select()
+          end,
+          desc = "Select session",
+        },
+        {
+          "<leader>ql",
+          function()
+            require("persistence").load({ last = true })
+          end,
+          desc = "Restore last session",
+        },
+        {
+          "<leader>qd",
+          function()
+            require("persistence").stop()
+          end,
+          desc = "Don't save current session",
+        },
+      },
+    },
+
     -- =============================================================================
     -- DOCUMENTATION AND NOTES
     -- =============================================================================
